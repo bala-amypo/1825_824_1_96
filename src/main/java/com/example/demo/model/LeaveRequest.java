441fda986be5c503a691bbf731e4b2928ec7036e
bala-amypo <bala@amypo.in>
2025-12-17 08:07:45 +0000

    User commit

--- src/main/java/com/example/demo/model/LeaveRequest.java
+++ src/main/java/com/example/demo/model/LeaveRequest.java
@@ -29,4 +29,11 @@ public class LeaveRequest{
     public LocalDate getStartDate() {return startDate;}
     public void setStartDate(LocalDate startDate) {this.startDate=startDate;}
     public LocalDate getEndDate() {return endDate;}
+    public void setEndDate(LocalDate endDate) {this.endDate=endDate;}
+    public String getType() {return type;}
+    public void setType(String type) {this.type=type;}
+    public String getStatus() {return status;}
+    public void setStatus(String status) {this.status=status;}
+    public String getReason() {return reason;}
+    public void setReason(String reason) {this.reason=reason;}
 }
