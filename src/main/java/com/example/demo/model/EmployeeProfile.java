aa20d17325676b06638003d85e61a24aefca84cf
bala-amypo <bala@amypo.in>
2025-12-17 06:23:24 +0000

    User commit

--- src/main/java/com/example/demo/model/EmployeeProfile.java
+++ src/main/java/com/example/demo/model/EmployeeProfile.java
@@ -3,4 +3,21 @@ package com.example.demo.model;
 import jakarta.persistance.Entity;
 import jakarta.persistance.Table;
 import jakarta.persistance.Id;
-import jakarta.persistance.Gen
+import jakarta.persistance.GeneratedValue;
+import jakarta.persistance.GenerationType;
+import jakarta.persistance.Column;
+import jakarta.persistance.UniqueConstraint;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(
+    name = "employee_profile",
+    UniqueConstraints = {
+        @UniqueConstraint(columnNames = "employeeId"),
+        @UniqueConstraint(columnNames = "email")
+    }
+)
+public class EmployeeProfile{
+    @Id
+    @GeneratedValue(strategy=GenerationType)
+}
