5a4c4aee71517e9b49a8ad16736c3248de684d40
bala-amypo <bala@amypo.in>
2025-12-17 04:34:57 +0000

    User commit

--- src/main/java/com/example/demo/dto/EmployeeProfileDto.java
+++ src/main/java/com/example/demo/dto/EmployeeProfileDto.java
@@ -4,5 +4,10 @@ import lombok.Data;
 
 @Data
 public class EmployeeProfileDto{
-    
+    private Long id;
+    private String employeeId;
+    private String fullName;
+    private String email;
+    private String teamName;
+    private String role;
 }
