4ee6d1aeace8f1ab25c21e7e226bc94a81b934ff
bala-amypo <bala@amypo.in>
2025-12-17 08:12:45 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/TeamCapacityConfig.java
@@ -0,0 +1,23 @@
+package com.example.demo.model;
+
+import jakarta.persistance.Entity;
+import jakarta.persistance.Id;
+import jakarta.persistance.GeneratedValue;
+import jakarta.persistance.GenerationType;
+import jakarta.persistance.Colomn;
+
+@Entity
+public class TeamCapacityConfig{
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    @Colomn(unique=true)
+    private String teamName;
+
+    private Integer totalHeadcount;
+    private Integer minCapacityPercent;
+
+    public Long getId() {return id;}
+    public String getTeamName()
+}
