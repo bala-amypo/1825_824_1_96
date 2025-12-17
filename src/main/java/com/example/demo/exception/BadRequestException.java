b95cd3eabaaa48dca41fbdde891eb81a0a2b4efb
bala-amypo <bala@amypo.in>
2025-12-17 10:48:19 +0000

    User commit

--- src/main/java/com/example/demo/exception/BadRequestException.java
+++ src/main/java/com/example/demo/exception/BadRequestException.java
@@ -1,9 +1,4 @@
 package com.exmaple.demo.exception;
 
-import java.lang;
-
-public class BadRequestException extends RuntimeException{
-    public BadRequestException(String message){
-        super(message);
-    }
+public class BadRequestException {
 }
