9a18e3730992e6476f86aa1010d5a07e806e23a5
bala-amypo <bala@amypo.in>
2025-12-18 08:10:03 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/LeaveRequestServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/LeaveRequestServiceImpl.java
@@ -2,4 +2,16 @@ package com.example.demo.service.impl;
 
 import org.springframework.stereotype.Service;
 import com.example.demo.service.LeaveRequestService;
-import com
+import com.example.demo.repository.LeaveRequestRepository;
+import com.example.demo.repository.EmployeeProfileRepository;
+import com.example.demo.model.LeaveRequest;
+import com.example.demo.model.EmployeeProfile;
+import java.time.LocalDate;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Service
+public class LeaveRequestServiceImpl implements LeaveRequestService{
+    @Autowired
+    private LeaveRequestRepository repo;
+}
