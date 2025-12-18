6ab1f67820a0722d46925fcc2428466de4c02839
bala-amypo <bala@amypo.in>
2025-12-18 09:20:04 +0000

    User commit

--- src/main/java/com/example/demo/security/JwtTokenProvider.java
+++ src/main/java/com/example/demo/security/JwtTokenProvider.java
@@ -8,5 +8,11 @@ import java.util.Date;
 @Component
 public class JwtTokenProvider{
     private final String SECRET="mysecretkey";
-    private final long EXPIRATION=
+    private final long EXPIRATION=86400000;
+    public String generateToken(String username){
+        return Jwts.builder().setSubject(username).setExpiration(new Date(System.currentTimeMillis()+EXPIRATION)).signWith(SignatureAlgorithm.HS256, SECRET).compact();
+    }
+    public String extractUserName(String token){
+        return Jwts.parser().setSigningKey(SECRET).parse
+    }
 }
