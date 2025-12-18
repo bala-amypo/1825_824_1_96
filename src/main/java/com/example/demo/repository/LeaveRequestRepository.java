43c2add9ac4c6888f5983e504dc0978327a926a1
bala-amypo <bala@amypo.in>
2025-12-18 04:37:08 +0000

    User commit

--- src/main/java/com/example/demo/repository/LeaveRequestRepository.java
+++ src/main/java/com/example/demo/repository/LeaveRequestRepository.java
@@ -9,15 +9,16 @@ import java.util.List;
 
 public interface LeaveRequestRepository extends JpaRepository<LeaveRequest, Long>{
     List<LeaveRequest> findByEmployee(EmployeeProfile employee);
-    @Query("""
-    select 1 from LeaveRequest 1
-    where 1.employee.teamName = :teamName
-    and 1.status = 'APPROVED'
-    and 1.startDate<= :end
-    and 1.endDate>= :start
-    """)
+    @Query(
+    "select 1 from LeaveRequest 1"+
+    "where 1.employee.teamName = :teamName" +
+    "and 1.status = 'APPROVED' "+
+    "and 1.startDate<= :end" +
+    "and 1.endDate>= :start"
+    )
     List<LeaveRequest> findApprovedOverlappingForTeam(
         @Param("teamName") String teamName,
         @Param("start") LocalDate start,
-        @Param("end") LocalDate end);
+        @Param("end") LocalDate end
+    );    
 }
