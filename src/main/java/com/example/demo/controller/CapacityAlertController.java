439bd9e57ca89048bec31c3b4e306e49456ca918
bala-amypo <bala@amypo.in>
2025-12-18 10:00:03 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/CapacityAlertController.java
@@ -0,0 +1,6 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.example.demo.service.CapacityAnalysisService;
+import com.example.demo.dto.CapacityAnalysisResult
