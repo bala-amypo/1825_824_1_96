bfee5f38d4d0a03683157a354e107cd2940612b4
bala-amypo <bala@amypo.in>
2025-12-18 09:58:01 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,3 +1,17 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.example.demo.dto.AuthRequest;
+import com.example.demo.dto.AuthResponse;
+import com.example.demo.service.AuthService;
+
+@RestController
+public class AuthController{
+    @Autowired
+    AuthService service;
+    @PostMapping("/login")
+    public AuthResponse login(@RequestBody AuthRequest req){
+        return service.authenticate(req);
+    }
+}
