d1fdc5152efaad1b079e3d2156be2645c3e3f05f
bala-amypo <bala@amypo.in>
2025-12-18 06:32:08 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/EmployeeProfileServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/EmployeeProfileServiceImpl.java
@@ -1,10 +1,11 @@
 package com.example.demo.service.impl;
 
-import com.example.demo.dto.EmployeeProfileDto;
-import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.model.EmployeeProfile;
 import com.example.demo.repository.EmployeeProfileRepository;
 import com.example.demo.service.EmployeeProfileService;
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
 import java.util.List;
-import java.util.stream.Collectors;
+
+@Service
+public class EmployeeProfileService
