49b698deb654695f2e5445770a677a32cbadf741
bala-amypo <bala@amypo.in>
2025-12-18 08:27:30 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/TeamCapacityConfigServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/TeamCapacityConfigServiceImpl.java
@@ -11,5 +11,16 @@ public class TeamCapacityConfigServiceImpl implements TeamCapacityConfigService{
     @Autowired
     private TeamCapacityConfigRepository repo;
     @Override
-    public TeamCapacityConfig createRule(TeamCapacityConfig rule)
+    public TeamCapacityConfig createRule(TeamCapacityConfig rule){
+        return repo.save(rule);
+    }
+    @Override
+    public TeamCapacityConfig updateRule(LOng id, TeamCapacityConfig rule){
+        rule.setId(id);
+        return repo.save(rule);
+    }
+    @Override
+    public TeamCapacityConfig getRuleByTeam(String teamName){
+        return repo.findByTeamName(teamName).orElse(null);
+    }
 }
