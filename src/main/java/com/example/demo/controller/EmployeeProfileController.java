8c07434e2db9dbd218af0422bd1b3cd0867a9556
bala-amypo <bala@amypo.in>
2025-12-18 09:35:03 +0000

    User commit

--- src/main/java/com/example/demo/controller/EmployeeProfileController.java
+++ src/main/java/com/example/demo/controller/EmployeeProfileController.java
@@ -15,5 +15,21 @@ public class EmployeeProfileController{
         return service.create(emp);
     }
     @PutMapping("/employee/{id}")
-    public Employee
+    public EmployeeProfile update(@PathVariable Long id, @RequestBody EmployeeProfile emp){
+        return service.update(id,emp);
+    }
+    @PutMapping("/employee/{id}/deactivate")
+    public void deactivate(@PathVariable Long id){
+        service.deactivate(id);
+    }
+    @GetMapping("/employee/{id}")
+    public EmployeeProfile getById(@PathVariable Long id){
+        return service.getById(id);
+    }
+    @GetMapping("/employee/team/{team}")
+    public List<EmployeeProfile> getTeam(@PathVariable String team){
+        return service.getByTeam(team);
+    }
+    @GetMapping("/employees")
+    public List<>
 }
