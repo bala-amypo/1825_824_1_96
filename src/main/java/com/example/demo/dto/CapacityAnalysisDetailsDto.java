59c7a81c754b6202e25ee9859056256fd27da186
bala-amypo <bala@amypo.in>
2025-12-18 10:35:03 +0000

    User commit

--- src/main/java/com/example/demo/dto/CapacityAnalysisDetailsDto.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.dto;
-
-import java.util.Map;
-import java.time.LocalDate;
-
-public class CapacityAnalysisDetailsDto{
-    private boolean risky;
-    private Map<LocalDate, Double> capacityByDate;
-}
