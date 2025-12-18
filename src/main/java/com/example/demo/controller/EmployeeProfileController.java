947a6ce0da529af584e0048bfa06c5eee6f0c2e9
bala-amypo <bala@amypo.in>
2025-12-18 09:30:03 +0000

    User commit

--- src/main/java/com/example/demo/controller/EmployeeProfileController.java
+++ src/main/java/com/example/demo/controller/EmployeeProfileController.java
@@ -1,4 +1,19 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.*;
-import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.example.demo.model.EmployeeProfile;
+import com.example.demo.service.EmployeeProfileService;
+import java.util.List;
+
+@RestController
+public class EmployeeProfileController{
+    @Autowired
+    EmployeeProfileService service;
+    @PostMapping("/employee")
+    public EmployeeProfile add(@RequestBody EmployeeProfile emp){
+        return service.create(emp);
+    }
+    @PutMapping("/employee/{id}")
+    public Employee
+}
