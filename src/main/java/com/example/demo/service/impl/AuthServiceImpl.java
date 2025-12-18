e5016bdbca5db62bca2487040ec5b1713d4f35d1
bala-amypo <bala@amypo.in>
2025-12-18 09:00:03 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/AuthServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/AuthServiceImpl.java
@@ -7,4 +7,21 @@ import com.example.demo.dto.AuthResponse;
 import com.example.demo.model.UserAccount;
 import com.example.demo.security.JwtTokenProvider;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Service
+public class AuthServiceImpl implements AuthService{
+    @Autowired
+    private UserAccountRepository repo;
+    @Autowired 
+    private BCryptPasswordEncoder encoder;
+    @Autowired
+    private JwtTokenProvider jwt;
+    @Override
+    public AuthResponse authenticate(AuthRequest req){
+        UserAccount user=repo.findByUsername(req.getUsername()).orElse(null);
+        if(user!=null && encoder.matches(req.getPassword(), user.getPassword())){
+            String token=jwt.generateToken(user.getUsername());
+        }
+    }
+}
