15a239061469a2a053d9307212f007c0fdc943c9
bala-amypo <bala@amypo.in>
2025-12-19 06:30:59 +0000

    User commit

--- src/main/java/com/example/demo/controller/TeamCapacityConfigController.java
+++ src/main/java/com/example/demo/controller/TeamCapacityConfigController.java
@@ -1,9 +1,11 @@
 package com.example.demo.controller;
-import org.springframework.web.bind.annotation.Reque
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.example.demo.model.TeamCapacityConfig;
 import com.example.demo.service.TeamCapacityConfigService;
+
+
 
 @RestController
 public class TeamCapacityConfigController{
