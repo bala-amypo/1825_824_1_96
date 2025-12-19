33056c76e0b3464185efbe760e1561de5cf72b49
bala-amypo <bala@amypo.in>
2025-12-19 04:03:59 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/EmployeeProfileServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/EmployeeProfileServiceImpl.java
@@ -3,43 +3,11 @@ package com.example.demo.service.impl;
 import com.example.demo.model.EmployeeProfile;
 import com.example.demo.repository.EmployeeProfileRepository;
 import com.example.demo.service.EmployeeProfileService;
-import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
 public class EmployeeProfileServiceImpl implements EmployeeProfileService{
-
-    @Autowired
-    private EmployeeProfileRepository repo;
-
-    @Override
-    public EmployeeProfile create(EmployeeProfile employee){
-        return repo.save(employee);
-    } 
-    @Override
-    public EmployeeProfile update(Long id, EmployeeProfile employee){
-        employee.setId(id);
-        return repo.save(employee);
-    }
-    @Override
-    public void deactivate(Long id){
-        EmployeeProfile emp=repo.findById(id).orElse(null);
-        if(emp!=null){
-            emp.setActive(false);
-            repo.save(emp);
-        }
-    }
-    @Override
-    public EmployeeProfile getById(Long id){
-        return repo.findById(id).orElse(null);
-    }
-    @Override
-    public List<EmployeeProfile> getByTeam(String teamName){
-        return repo.findByTeamNameAndActiveTrue(teamName);
-    }
-    @Override
-    public List<EmployeeProfile> getAll(){
-        return repo.findAll();
-    }
+    
 }
