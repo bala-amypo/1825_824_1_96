0b1fb958554e6f7c89cd6cfe909331d2d889b53b
bala-amypo <bala@amypo.in>
2025-12-19 04:36:59 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/AuthServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/AuthServiceImpl.java
@@ -1,3 +1,13 @@
 package com.example.demo.service.impl;
 
-import com.example.demo.repository.
+import com.example.demo.repository.UserAccountRepository;
+import com.example.demo.model.UserAccount;
+import com.example.demo.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthServiceImpl implements AuthService{
+    @Autowired
+    UserAccountRepository repo;
+}
