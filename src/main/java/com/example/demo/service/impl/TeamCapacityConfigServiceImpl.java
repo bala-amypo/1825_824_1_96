8b1c7f2328f37ce5d0d3258e2e01d54b396f26f9
bala-amypo <bala@amypo.in>
2025-12-19 04:27:59 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/TeamCapacityConfigServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/TeamCapacityConfigServiceImpl.java
@@ -1,3 +1,14 @@
 package com.example.demo.service.impl;
 
-import com.example.demo.model.
+import com.example.demo.model.TeamCapacityConfig;
+import com.example.demo.repository.TeamCapacityConfigRepository;
+import com.example.demo.service.TeamCapacityConfigService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TeamCapacityConfigServiceImpl implements TeamCapacityConfigService{
+    @Autowired
+    TeamCapacityConfigRepository repo;
+    
+}
