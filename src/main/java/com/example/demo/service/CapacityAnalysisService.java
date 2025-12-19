f2b546f3cade6c7d09cade6ad14048e3cbb58e0f
bala-amypo <bala@amypo.in>
2025-12-19 03:59:25 +0000

    User commit

--- src/main/java/com/example/demo/service/CapacityAnalysisService.java
+++ src/main/java/com/example/demo/service/CapacityAnalysisService.java
@@ -2,3 +2,7 @@ package com.example.demo.service;
 
 import java.time.LocaDate;
 import java.util.Map;
+
+public interface CapacityAnalysisService{
+    Map<LocalDate, Double> analyze(String teamName, LocalDate start, LocalDate end);
+}
