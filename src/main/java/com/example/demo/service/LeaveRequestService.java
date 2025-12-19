c14f0acc7c2e18aadc87e1f5f632fedf6ceeca30
bala-amypo <bala@amypo.in>
2025-12-19 03:48:59 +0000

    User commit

--- src/main/java/com/example/demo/service/LeaveRequestService.java
+++ src/main/java/com/example/demo/service/LeaveRequestService.java
@@ -1,13 +1,10 @@
 package com.example.demo.service;
 
-import com.example.demo.dto.LeaveRequestDto;
+import com.example.demo.model.LeaveRequest;
 import java.time.LocalDate;
 import java.util.List;
 
 public interface LeaveRequestService{
-    LeaveRequestDto create(LeaveRequestDto dto);
-    void approve(Long id);
-    void reject(Long id);
-    List<LeaveRequestDto> getByEmployee(Long employeeId);
-    List<LeaveRequestDto> getOverlappingForTeam(String teamName, LocalDate start, LocalDate end);
+
+    LeaveRequest create( 
 }
