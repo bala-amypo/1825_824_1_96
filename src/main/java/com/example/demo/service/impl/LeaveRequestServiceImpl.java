d4c50642f906c613d0eab491673be6a133fb4fd1
bala-amypo <bala@amypo.in>
2025-12-19 04:15:59 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/LeaveRequestServiceImpl.java
@@ -0,0 +1,9 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.model.LeaveRequest;
+import com.example.demo.model.EmployeeProfile;
+import com.example.demo.repository.LeaveRequestRepository;
+import com.example.demo.repository.EmployeeProfileRepository;
+import com.example.demo.service.LeaveRequestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org
