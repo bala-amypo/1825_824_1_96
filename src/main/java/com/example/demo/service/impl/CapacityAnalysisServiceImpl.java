67ae1eb7fd889a2fb11087d1d3ba4343e27d08ee
bala-amypo <bala@amypo.in>
2025-12-19 04:45:58 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/CapacityAnalysisServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/CapacityAnalysisServiceImpl.java
@@ -2,4 +2,13 @@ package com.example.demo.service.impl;
 
 import com.example.demo.model.TeamCapacityConfig;
 import com.example.demo.model.LeaveRequest;
-import c
+import com.example.demo.repository.LeaveRequestRepository;
+import com.example.demo.repository.TeamCapacityConfigRepository;
+import com.example.demo.service.CapacityAnalysisService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.time.LocalDate;
+import java.util.*;
+
+@Service
+public class CapacityAnalysisServiceImpl implements 
