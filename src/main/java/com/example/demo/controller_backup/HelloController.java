b769178b66c2635df937d217392c8493dbb0a01b
bala-amypo <bala@amypo.in>
2025-12-20 07:42:55 +0000

    User commit

--- src/main/java/com/example/demo/controller_backup/HelloController.java
+++ src/main/java/com/example/demo/controller_backup/HelloController.java
@@ -4,10 +4,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class TestController {
+public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Spring Boot is working";
+        return "Spring Boot is running!";
     }
 }
