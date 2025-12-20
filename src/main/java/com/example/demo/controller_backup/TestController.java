ef2e888f1253289472d4b529f24ac4c4cbabedf5
bala-amypo <bala@amypo.in>
2025-12-20 07:36:55 +0000

    User commit

--- src/main/java/com/example/demo/controller_backup/TestController.java
+++ src/main/java/com/example/demo/controller_backup/TestController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    @GetMapping("/test")
-    public String test() {
-        return "Swagger working";
+    @GetMapping("/hello")
+    public String hello() {
+        return "Spring Boot is working";
     }
 }
