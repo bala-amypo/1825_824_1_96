dbac47d6047e44ce8552fe24fcb1f23488478192
bala-amypo <bala@amypo.in>
2025-12-20 07:15:55 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/EmployeeProfileServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/EmployeeProfileServiceImpl.java
@@ -3,41 +3,51 @@ package com.example.demo.service.impl;
 import com.example.demo.model.EmployeeProfile;
 import com.example.demo.repository.EmployeeProfileRepository;
 import com.example.demo.service.EmployeeProfileService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 
 @Service
-public class EmployeeProfileServiceImpl implements EmployeeProfileService{
-    @Autowired
-    EmployeeProfileRepository repo;
+public class EmployeeProfileServiceImpl implements EmployeeProfileService {
+
+    private final EmployeeProfileRepository repo;
+
+    public EmployeeProfileServiceImpl(EmployeeProfileRepository repo) {
+        this.repo = repo;
+    }
+
     @Override
-    public EmployeeProfile create(EmployeeProfile emp){
+    public EmployeeProfile create(EmployeeProfile emp) {
         return repo.save(emp);
     }
+
     @Override
-    public EmployeeProfile update(Long id, EmployeeProfile emp){
+    public EmployeeProfile update(Long id, EmployeeProfile emp) {
         emp.setId(id);
         return repo.save(emp);
     }
+
     @Override
-    public void deactivate(Long id){
-        Employee emp=repo.findById(id).orElse(null);
-        if(emp!=null){
+    public void deactivate(Long id) {
+        EmployeeProfile emp = repo.findById(id).orElse(null);
+        if (emp != null) {
             emp.setActive(false);
             repo.save(emp);
         }
     }
+
     @Override
-    public EmployeeProfile getById(Long id){
+    public EmployeeProfile getById(Long id) {
         return repo.findById(id).orElse(null);
     }
+
     @Override
-    public List<EmployeeProfile> getByTeam(String team){
+    public List<EmployeeProfile> getByTeam(String team) {
         return repo.findByTeamNameAndActiveTrue(team);
     }
+
     @Override
-    public List<EmployeeProfile> getAll(){
+    public List<EmployeeProfile> getAll() {
         return repo.findAll();
     }
 }
