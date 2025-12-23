8dde85c5de23d5772f0c3eb6bba388706bf1fa87
bala-amypo <bala@amypo.in>
2025-12-23 04:28:25 +0000

    User commit

--- src/main/java/com/example/demo/model/UserAccount.java
+++ src/main/java/com/example/demo/model/UserAccount.java
@@ -21,4 +21,19 @@ public class UserAccount{
 
     @OneToOne
     @JoinColumn(name="employee_id")
+    private EmployeeProfile employeeProfile;
+
+    public Long getId(){
+        return id;
+    }
+    public void setId(Long id){
+        this.id=id;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email){
+        this.email=email;
+    }
+    public String getPassword()
 }
