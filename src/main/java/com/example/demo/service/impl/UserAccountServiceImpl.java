f4ae34544e7ec94e3e8877f18ff9b019a4990522
bala-amypo <bala@amypo.in>
2025-12-23 04:49:26 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/UserAccountServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserAccountServiceImpl.java
@@ -4,4 +4,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import com.example.demo.model.UserAccount;
-import com.example.
+import com.example.demo.repository.UserAccountRepository;
+import com.example.demo.service.UserAccountService;
+
+@Service
+public class UserAccountServiceImpl implements UserAccountService;
