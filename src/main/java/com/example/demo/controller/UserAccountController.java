7fc7df3223408af1c611b9a1187fcd90cd4009ca
bala-amypo <bala@amypo.in>
2025-12-23 04:58:25 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/UserAccountController.java
@@ -0,0 +1,6 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Requestb
