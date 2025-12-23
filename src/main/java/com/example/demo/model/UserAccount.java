288296b37d576b7109537ec7cdbaed91a4d65826
bala-amypo <bala@amypo.in>
2025-12-23 04:31:25 +0000

    User commit

--- src/main/java/com/example/demo/model/UserAccount.java
+++ src/main/java/com/example/demo/model/UserAccount.java
@@ -35,5 +35,20 @@ public class UserAccount{
     public void setEmail(String email){
         this.email=email;
     }
-    public String getPassword()
+    public String getPassword(){
+        return password;
+    }
+    public void setPassword(String password){
+        this.password=password;
+    }
+    public String getRole(){
+        return role;
+    }
+    public void setRole(String role){
+        this.role=role;
+    }
+    public EmployeeProfile getEmployeeProfile(){
+        return employeeProfile;
+    }
+    public void setEmployeeProfile(EmployeeProfile employeeProfile)
 }
