bfc631f5e2cebc95ed80cd1ef8139b227f80a67b
bala-amypo <bala@amypo.in>
2025-12-23 05:34:25 +0000

    User commit

--- src/main/java/com/example/demo/controller/CapacityAlertController.java
+++ src/main/java/com/example/demo/controller/CapacityAlertController.java
@@ -4,4 +4,16 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.example.demo.model.CapacityAlert;
+import com.example.demo.service.CapacityAlertService;
+import java.util.List;
+
+@RestController
+public class CapacityAlertController{
+    @Autowired
+    private CapacityAlertService service;
+    @PostMapping("/capacity-alerts")
+    public CapacityAlert create()
+}
