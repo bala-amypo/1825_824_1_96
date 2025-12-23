5178294f230c26d7ffc53e28ec1d73b860284655
bala-amypo <bala@amypo.in>
2025-12-23 05:25:25 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/CapacityAlertServiceImpl.java
@@ -0,0 +1,6 @@
+package com.example.demo.service.impl;
+
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+import com.example.demo.model.Capacitya
