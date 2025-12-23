a8c62eec0747ae5397683751a7b766a8d87f54b4
bala-amypo <bala@amypo.in>
2025-12-23 04:25:25 +0000

    User commit

--- src/main/java/com/example/demo/model/UserAccount.java
+++ src/main/java/com/example/demo/model/UserAccount.java
@@ -6,4 +6,19 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.JoinColumn;
-import jakarta
+import jakarta.persistence.Column;
+
+@Entity
+public class UserAccount{
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique=true, nullable=false)
+    private String email;
+    private String password;
+    private String role;
+
+    @OneToOne
+    @JoinColumn(name="employee_id")
+}
