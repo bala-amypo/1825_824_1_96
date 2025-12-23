37988a1915adddd6ddc4e655210a89b618563d3a
bala-amypo <bala@amypo.in>
2025-12-23 05:31:25 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/CapacityAlertController.java
@@ -0,0 +1,7 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.
