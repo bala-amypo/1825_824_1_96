bc5f99d744685501744c6f48505ce93cc0259675
bala-amypo <bala@amypo.in>
2025-12-24 04:17:06 +0000

    User commit

---
+++ src/main/java/com/example/demo/dto/EmployeeProfileDto.java
@@ -0,0 +1,9 @@
+package com.example.demo.dto;
+
+public class EmployeeProfileDto{
+    private Long id;
+    private String employeeId;
+    private String fullName;
+    private String email;
+    private String teamName
+}
