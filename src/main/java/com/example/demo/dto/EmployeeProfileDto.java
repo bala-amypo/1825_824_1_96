50e10569bdbb842a6fa8150a8c7b63fb1edf9ee3
bala-amypo <bala@amypo.in>
2025-12-24 04:20:06 +0000

    User commit

--- src/main/java/com/example/demo/dto/EmployeeProfileDto.java
+++ src/main/java/com/example/demo/dto/EmployeeProfileDto.java
@@ -5,5 +5,20 @@ public class EmployeeProfileDto{
     private String employeeId;
     private String fullName;
     private String email;
-    private String teamName
+    private String teamName;
+    private String role;
+
+    public Long getId(){
+        return id;
+    }
+    public void setId(Long id){
+        this.id=id;
+    }
+    public String getEmployeeId(){
+        return employeeId;
+    }
+    public void setEmployeeId(String employeeId){
+        this.employeeId=employeeId;
+    }
+    public String 
 }
