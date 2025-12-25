a9be4777a76da428e1eefbbb9503100ae1b027a9
bala-amypo <bala@amypo.in>
2025-12-25 16:52:25 +0000

    User commit

--- src/main/java/com/example/demo/controller/CapacityAlertController.java
+++ src/main/java/com/example/demo/controller/CapacityAlertController.java
@@ -1,25 +1,25 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.beans.factory.annotation.Autowired;
 import com.example.demo.model.CapacityAlert;
 import com.example.demo.service.CapacityAlertService;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
-public class CapacityAlertController{
-    @Autowired
-    private CapacityAlertService service;
-    @PostMapping("/capacity-alerts")
-    public CapacityAlert create(@RequestBody CapacityAlert alert){
-        return service.save(alert);
+@RequestMapping("/capacity-alerts")
+public class CapacityAlertController {
+
+    private final CapacityAlertService service;
+
+    public CapacityAlertController(CapacityAlertService service) {
+        this.service = service;
     }
-    @GetMapping("/capacity-alerts/team/{teamName}")
-    public List<CapacityAlert> getByTeam(@PathVariable String teamName){
+
+    @GetMapping("/team/{teamName}")
+    public List<CapacityAlert> getByTeam(
+            @PathVariable String teamName
+    ) {
         return service.getByTeam(teamName);
     }
 }
