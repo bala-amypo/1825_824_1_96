c2a0dde9e5cba7d0c23597035df2885878f68b72
bala-amypo <bala@amypo.in>
2025-12-25 15:07:28 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/CapacityAlertServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/CapacityAlertServiceImpl.java
@@ -1,22 +1,25 @@
 package com.example.demo.service.impl;
 
-import org.springframework.stereotype.Service;
-import org.springframework.beans.factory.annotation.Autowired;
-import java.util.List;
 import com.example.demo.model.CapacityAlert;
 import com.example.demo.repository.CapacityAlertRepository;
 import com.example.demo.service.CapacityAlertService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
-public class CapacityAlertServiceImpl implements CapacityAlertService{
-    @Autowired
-    private CapacityAlertRepository repo;
-    @Override
-    public CapacityAlert save(CapacityAlert alert){
-        return repo.save(alert);
+public class CapacityAlertServiceImpl implements CapacityAlertService {
+
+    private final CapacityAlertRepository repo;
+
+    public CapacityAlertServiceImpl(CapacityAlertRepository repo) {
+        this.repo = repo;
     }
+
     @Override
-    public List<CapacityAlert> getByTeam(String teamName){
-        return repo.findByTeamName(teamName);
+    public List<CapacityAlert> getByTeam(String teamName) {
+        return repo.findByTeamName(teamName)
+                .map(List::of)
+                .orElse(List.of());
     }
 }
