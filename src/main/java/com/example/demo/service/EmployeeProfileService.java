0f06db4fecb15c97b65e78a2938d038948f054cb
bala-amypo <bala@amypo.in>
2025-12-25 16:38:07 +0000

    User commit

--- src/main/java/com/example/demo/service/EmployeeProfileService.java
+++ src/main/java/com/example/demo/service/EmployeeProfileService.java
@@ -1,20 +1,12 @@
-package com.example.demo.service;
-
-import com.example.demo.model.EmployeeProfile;
-
-import java.util.List;
-
 public interface EmployeeProfileService {
 
-    EmployeeProfile create(EmployeeProfile employee);
+    EmployeeProfile save(EmployeeProfile profile);
 
-    EmployeeProfile update(Long id, EmployeeProfile employee);
+    EmployeeProfile update(Long id, EmployeeProfile profile);
 
     void deactivate(Long id);
 
     EmployeeProfile getById(Long id);
 
-    List<EmployeeProfile> getByTeam(String teamName);
-
-    List<EmployeeProfile> getAll();
+    List<EmployeeProfile> getByTeam(String team);
 }
