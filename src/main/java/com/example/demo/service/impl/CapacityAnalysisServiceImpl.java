d065c8a5293e3ec5997041ed2758e522d884eeb1
bala-amypo <bala@amypo.in>
2025-12-25 16:08:35 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/CapacityAnalysisServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/CapacityAnalysisServiceImpl.java
@@ -9,7 +9,8 @@ import java.time.LocalDate;
 import java.util.List;
 
 @Service
-public class CapacityAnalysisServiceImpl implements CapacityAnalysisService {
+public class CapacityAnalysisServiceImpl
+        implements CapacityAnalysisService {
 
     @Override
     public boolean isCapacityExceeded(
