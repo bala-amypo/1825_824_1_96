21d3a2797a3828d15a25ff21c16c1ac5d1b98f1f
bala-amypo <bala@amypo.in>
2025-12-26 09:53:21 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/AuthServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/AuthServiceImpl.java
@@ -1,15 +1,10 @@
 package com.example.demo.service.impl;
 
-import org.springframework.stereotype.Service;
 import com.example.demo.dto.AuthRequest;
 import com.example.demo.dto.AuthResponse;
+import com.example.demo.model.UserAccount;
+import com.example.demo.repository.UserAccountRepository;
 import com.example.demo.service.AuthService;
-
-@Service
-public class AuthServiceImpl implements AuthService{
-
-    @Override
-    public AuthResponse authenticate(AuthRequest request){
-        return new AuthResponse("dummy-token");
-    }
-}
+import com.example.demo.security.JwtTokenProvider;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired
