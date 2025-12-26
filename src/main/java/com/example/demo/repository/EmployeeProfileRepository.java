60ffb561869d2b27b8daaa5243d78deaaa98f09b
bala-amypo <bala@amypo.in>
2025-12-26 05:14:19 +0000

    User commit

--- src/main/java/com/example/demo/repository/EmployeeProfileRepository.java
+++ src/main/java/com/example/demo/repository/EmployeeProfileRepository.java
@@ -1,9 +1,7 @@
 package com.example.demo.repository;
-
 import com.example.demo.model.EmployeeProfile;
 import java.util.List;
 import java.util.Optional;
-
 public interface EmployeeProfileRepository {
     EmployeeProfile save(EmployeeProfile employee);
     Optional<EmployeeProfile> findById(Long id);
