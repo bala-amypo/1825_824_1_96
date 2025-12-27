adfb97505b21f62c3876b39b9de1a2cfa077f531
bala-amypo <bala@amypo.in>
2025-12-27 05:38:32 +0000

    User commit

--- src/main/java/com/example/demo/model/CapacityAlert.java
+++ src/main/java/com/example/demo/model/CapacityAlert.java
@@ -1,3 +1,18 @@
 package com.example.demo.model;
 
-import jakarta.persistence.
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+
+@Entity
+@Table(name="capcity_alerts")
+public class CapacityAlert{
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    
+}
