2c6a2146b21dcb5cdbc5cbe5a1e954e91304b5b6
bala-amypo <bala@amypo.in>
2025-12-27 04:29:32 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/CapacityAlertServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/CapacityAlertServiceImpl.java
@@ -1,16 +1,18 @@
 package com.example.demo.service.impl;
 import com.example.demo.model.CapacityAlert;
 import com.example.demo.repository.CapacityAlertRepository;
-import com.example.demo.service.Capacity
+import com.example.demo.service.CapacityAlertService;
+import org.springframework.stereotype.Service;
+import java.util.List;
 
-public class CapacityAlertServiceImpl {
-    private final CapacityAlertRepository repository;
-    
-    public CapacityAlertServiceImpl(CapacityAlertRepository repository) {
-        this.repository = repository;
+@Service
+public class CapacityAlertServiceImpl implements CapacityAlertService{
+
+    private final CapacityAlertRepository capacityAlertRepository;
+
+    public CapacityAlertServiceImpl(CapacityAlertRepository capacityAlertRepository){
+        this.capacityAlertRepository=capacityAlertRepository;
     }
-    
-    public CapacityAlert save(CapacityAlert alert) {
-        return repository.save(alert);
-    }
+    @Override
+    public CapacityAL
 }
