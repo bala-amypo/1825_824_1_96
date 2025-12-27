ec91385796721e95ffd86e6cf1ff0730b75241b0
bala-amypo <bala@amypo.in>
2025-12-27 04:02:32 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/AuthServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/AuthServiceImpl.java
@@ -9,48 +9,5 @@ import com.example.demo.service.AuthService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @Service
-public class AuthServiceImpl implements AuthService {
-
-    private UserAccountRepository userAccountRepository;
-    private BCryptPasswordEncoder passwordEncoder;
-    private JwtTokenProvider jwtTokenProvider;
-
-    // REQUIRED BY TEST CASES
-    public AuthServiceImpl(
-            UserAccountRepository userAccountRepository,
-            BCryptPasswordEncoder passwordEncoder,
-            JwtTokenProvider jwtTokenProvider
-    ) {
-        this.userAccountRepository = userAccountRepository;
-        this.passwordEncoder = passwordEncoder;
-        this.jwtTokenProvider = jwtTokenProvider;
-    }
-
-    // REQUIRED BY SPRING
-    public AuthServiceImpl() {
-    }
-
-    @Override
-    public AuthResponse authenticate(AuthRequest request) {
-
-        Optional<UserAccount> optionalUser =
-                userAccountRepository.findByEmail(request.getUsername());
-
-        if (optionalUser.isEmpty()) {
-            throw new RuntimeException("User not found");
-        }
-
-        UserAccount user = optionalUser.get();
-
-        if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
-            throw new RuntimeException("Invalid credentials");
-        }
-
-        String token = jwtTokenProvider.generateToken(user);
-
-        return new AuthResponse(user.getId(), token);
-    }
-}
+public class 
