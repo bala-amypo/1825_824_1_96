057936d994a21e9a006b277cdb8be321fb9935e7
bala-amypo <bala@amypo.in>
2025-12-27 04:39:09 +0000

    User commit

--- src/main/java/com/example/demo/repository/CapacityAlertRepository.java
+++ src/main/java/com/example/demo/repository/CapacityAlertRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface Capacity
+public interface CapacityAlertRepository extends JpaRepository<CapacityAlert, Long>{
+}
