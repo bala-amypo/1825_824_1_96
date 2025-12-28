2a3f81e38ba4ed8617ca7b3ea4b94e0ab65012b9
bala-amypo <bala@amypo.in>
2025-12-28 09:21:54 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/AuthServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/AuthServiceImpl.java
@@ -1,14 +1,19 @@
 package com.example.demo.service.impl;
 
 import com.example.demo.repository.UserAccountRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthServiceImpl {
 
-    private final UserAccountRepository repository;
+    private final UserAccountRepository userRepo;
+    private final BCryptPasswordEncoder passwordEncoder;
 
-    public AuthServiceImpl(UserAccountRepository repository) {
-        this.repository = repository;
+    // ✅ MATCHES TEST EXPECTATION
+    public AuthServiceImpl(UserAccountRepository userRepo,
+                           BCryptPasswordEncoder passwordEncoder) {
+        this.userRepo = userRepo;
+        this.passwordEncoder = passwordEncoder;
     }
 }
