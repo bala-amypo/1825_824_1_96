32e4d680dfecef1155fa9776105f48bb85a094ee
bala-amypo <bala@amypo.in>
2025-12-29 05:20:01 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/AuthServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/AuthServiceImpl.java
@@ -36,7 +36,7 @@ public class AuthServiceImpl implements AuthService {
             throw new BadRequestException("Invalid email or password");
         }
 
-        String token = jwtTokenProvider.generateToken(user.getEmail());
+        String token = jwtTokenProvider.generateToken((String) user.getEmail());
 
         return new AuthResponse(
                 user.getId(),
