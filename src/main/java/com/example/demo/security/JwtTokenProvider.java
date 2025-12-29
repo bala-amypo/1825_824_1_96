05e7e8d5f103d31b3a864e4a7abb86a02f27ae5a
bala-amypo <bala@amypo.in>
2025-12-29 05:18:55 +0000

    User commit

--- src/main/java/com/example/demo/security/JwtTokenProvider.java
+++ src/main/java/com/example/demo/security/JwtTokenProvider.java
@@ -1,6 +1,6 @@
 package com.example.demo.security;
 
-import com.example.model.demo.UserAccount;
+import com.example.demo.model.UserAccount;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
