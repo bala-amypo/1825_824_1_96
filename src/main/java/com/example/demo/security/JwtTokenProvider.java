287d638a302b2172187509b32a3cc855fe2947b9
bala-amypo <bala@amypo.in>
2025-12-29 04:29:02 +0000

    User commit

--- src/main/java/com/example/demo/security/JwtTokenProvider.java
+++ src/main/java/com/example/demo/security/JwtTokenProvider.java
@@ -6,6 +6,9 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import java.util.Date;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+
 
 @Component
 public class JwtTokenProvider{
@@ -16,12 +19,9 @@ public class JwtTokenProvider{
     @Value("${jwt.expiration}")
     private long jwtExpiration;
 
-    public String generateToken(String username){
-        return Jwts.builder()
-               .setSubject(username)
-               .setIssuedAt(new Date())
-               .setExpiration(new Date(System.currentTimeMillis() + jwtExpiration))
-               .signWith(SignatureAlgorithm.HS512, jwtSecret)
-               .compact();
+    private Key getSigninKey(){
+        return Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8));
     }
+    
+
 }
