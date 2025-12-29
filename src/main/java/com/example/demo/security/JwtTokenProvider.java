8915c445836c8cb402b0b3ca9eb20db7438aa65c
bala-amypo <bala@amypo.in>
2025-12-29 04:16:58 +0000

    User commit

--- src/main/java/com/example/demo/security/JwtTokenProvider.java
+++ src/main/java/com/example/demo/security/JwtTokenProvider.java
@@ -1,3 +1,15 @@
 package com.example.demo.security;
 
-import io
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider{
+
+    @Value("${jwt.secret}")
+    
+}
