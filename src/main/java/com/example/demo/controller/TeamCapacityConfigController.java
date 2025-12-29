a8afa01aca48d2d502ba5c30f1391d4c6ac27235
bala-amypo <bala@amypo.in>
2025-12-29 06:36:55 +0000

    User commit

--- src/main/java/com/example/demo/controller/TeamCapacityConfigController.java
+++ src/main/java/com/example/demo/controller/TeamCapacityConfigController.java
@@ -2,38 +2,8 @@ package com.example.demo.controller;
 
 import com.example.demo.model.TeamCapacityConfig;
 import com.example.demo.service.TeamCapacityConfigService;
+import org.springframework.http.
 import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping("/capacity-config")
-public class TeamCapacityConfigController {
 
-    private final TeamCapacityConfigService service;
 
-    public TeamCapacityConfigController(
-            TeamCapacityConfigService service) {
-        this.service = service;
-    }
-
-    @PostMapping
-    public TeamCapacityConfig create(
-            @RequestBody TeamCapacityConfig config
-    ) {
-        return service.createRule(config);
-    }
-
-    @PutMapping("/{id}")
-    public TeamCapacityConfig update(
-            @PathVariable Long id,
-            @RequestBody TeamCapacityConfig config
-    ) {
-        return service.updateRule(id, config);
-    }
-
-    @GetMapping("/team/{teamName}")
-    public TeamCapacityConfig getByTeam(
-            @PathVariable String teamName
-    ) {
-        return service.getRuleByTeam(teamName);
-    }
-}
