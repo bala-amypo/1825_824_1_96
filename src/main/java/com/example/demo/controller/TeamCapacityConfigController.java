212782df488161784202fbb96f72e12db280d09b
bala-amypo <bala@amypo.in>
2025-12-29 06:39:55 +0000

    User commit

--- src/main/java/com/example/demo/controller/TeamCapacityConfigController.java
+++ src/main/java/com/example/demo/controller/TeamCapacityConfigController.java
@@ -2,8 +2,24 @@ package com.example.demo.controller;
 
 import com.example.demo.model.TeamCapacityConfig;
 import com.example.demo.service.TeamCapacityConfigService;
-import org.springframework.http.
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/capacity-config")
+public class TeamCapacityConfigController{
+
+    private final TeamCapacityConfigService service;
+
+    public TeamCapacityConfigController(TeamCapacityConfigService service){
+        this.service=service;
+    }
+    @PostMapping
+    public ResponseEntity<TeamCapacityConfig> create(@RequestBody TeamCapacityConfig config){
+        return 
+    }
+}
 
 
 
